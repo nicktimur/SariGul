35ad6d4b863f14d9e0ea2c43d3af09927ce5483e
nicktimur <timur1karakas09@gmail.com>
2023-12-28 17:51:55 +0300

    kamera iyileştirmesi

---
+++ SariGul/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float offset;
+    [SerializeField] private float camSpeed;
+    private float lookAhead;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (-offset * player.localScale.x), Time.deltaTime * camSpeed);  }
+    
+}
