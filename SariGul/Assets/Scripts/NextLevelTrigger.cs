66b2b5ce0d7f1da25e9c50bcc136abdcae25d213
Timur Karakaş <timur1karakas09@gmail.com>
2024-01-11 15:00:12 +0300

    scene2 denemeleri

--- SariGul/Assets/Scripts/NextLevelTrigger.cs
+++ SariGul/Assets/Scripts/NextLevelTrigger.cs
@@ -8,9 +8,17 @@ public class NextLevelTrigger : MonoBehaviour
     private LevelLoader levelLoader;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+            levelLoader.LoadNextLevel();
+
+            Transform player = collision.GetComponent<Player>().transform;
+            Transform camera = GameObject.Find("Main Camera").transform;
+            player.position = new Vector3(20.65637f, 6.800001f, 0.1985453f);
+            camera.position = new Vector3(21.71f, 8.9f, -5);
+
         }
+
     }
 }
